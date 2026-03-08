7d99f42e11245a673f56da99f7473cc2fe693849
Benjamin De Kosnik <bkoz@gnu.org>
2026-03-07 16:05:32 -0800

    update generated tables and graphs

--- scripts/generate-graphs.sh
+++ scripts/generate-graphs.sh
@@ -1,6 +1,6 @@
 
-./a60-graph-meta-collection.exe ../data.ahsoka "Ahsoka, Ackolyte, Others" "" ""
+./a60-graph-meta-collection.exe ../data.ahsoka "Ahsoka, Ackolyte, Others" "" "" false
 
-./a60-graph-meta-collection.exe ../data.v2 "SWU" "" ""
-./a60-graph-meta-collection.exe ../data.v2 "Andor S1,S2" "" "andor"
-./a60-graph-meta-collection.exe ../data.v2 "Mandalorian S1,S2,S3" "" "mandalorian"
+./a60-graph-meta-collection.exe ../data.v2 "SWU" "" "" false
+./a60-graph-meta-collection.exe ../data.v2 "Andor S1,S2" "" "andor" true
+./a60-graph-meta-collection.exe ../data.v2 "Mandalorian S1,S2,S3" "" "mandalorian" true
