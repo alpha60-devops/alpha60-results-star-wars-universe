8cdb7f52124538c342a01405f432d4e6c4e60afb
Benjamin De Kosnik <bkoz@gnu.org>
2026-03-20 18:43:53 -0700

    update to v4 outputs

--- scripts/geo-compare.sh
+++ scripts/geo-compare.sh
@@ -1,4 +1,4 @@
 
-#/home/bkoz/src/alpha60-results/scripts/geojson-statistical-analysis.py --swarm downloaders --field size --geopivot country_code --prefix andor-112-2022-to-2025 ./data/andor-112-2022-cumulative.geojson ./data/andor-112-2025-cumulative.geojson
+/home/bkoz/src/alpha60-results/scripts/geojson-statistical-analysis.py --swarm downloaders --field size --geopivot country_code --prefix andor-112-2022-to-2025 ./data/andor-112-2022-cumulative.geojson ./data/andor-112-2025-cumulative.geojson
 
 /home/bkoz/src/alpha60-results/scripts/geojson-statistical-analysis.py --swarm downloaders --field size --prefix andor-112-2022-to-2025 ./data/andor-112-2022-cumulative.geojson ./data/andor-112-2025-cumulative.geojson
