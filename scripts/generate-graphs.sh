a2f58068ee4cf9d63e8ef58a6556031975fd1161
Benjamin De Kosnik <bkoz@gnu.org>
2026-03-30 16:23:05 -0700

    truncate andor-only data length to 26

--- scripts/generate-graphs.sh
+++ scripts/generate-graphs.sh
@@ -1,4 +1,4 @@
 ./a60-meta-collection.exe ../data.ahsoka "Ahsoka, Ackolyte, Others" "" "" 0
 ./a60-meta-collection.exe ../data "SWU" "" "" 0
-./a60-meta-collection.exe ../data "Andor S1,S2" "" "andor" 1
+./a60-meta-collection.exe ../data "Andor S1,S2" "" "andor" 0
 ./a60-meta-collection.exe ../data "Mandalorian S1,S2,S3" "" "mandalorian" 1
