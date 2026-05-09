74623904883aa2661a046b0f6750693c5fb9f443
Benjamin De Kosnik <bkoz@gnu.org>
2026-05-08 20:03:33 -0700

    add country

--- scripts/generate-graphs.sh
+++ scripts/generate-graphs.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
+
 ./a60-meta-collection.exe ../data.ahsoka "Ahsoka, Ackolyte, Others" "" "" 0
 ./a60-meta-collection.exe ../data "SWU" "" "" 0
 ./a60-meta-collection.exe ../data "Andor S1,S2" "" "andor" 0
 ./a60-meta-collection.exe ../data "Mandalorian S1,S2,S3" "" "mandalorian" 1
+
+./a60-meta-collection.country.exe ../data/andor-112-2022-week.json "Andor 112 2022"
+
+./a60-meta-collection.country.exe ../data/andor-112-2025-week.json "Andor 112 2025"
